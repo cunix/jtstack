351b0db02614fd02454033962bfe0b610a6175f7
Wincent.Wang <cunix@sina.cn>
2016-05-24 17:41:21 +0800

    add files

---
+++ jtstack-nio/src/main/java/com/phei/netty/nio/NioUserStep.java
@@ -0,0 +1,33 @@
+package com.phei.netty.nio;
+
+import java.io.IOException;
+import java.nio.channels.ServerSocketChannel;
+
+public class NioUserStep {
+
+	
+	public static void main(String[] args) throws IOException {
+		//步骤一：
+		ServerSocketChannel acceptorSrv=ServerSocketChannel.open();
+		//
+		
+		//
+		
+		
+		//
+		
+		///
+		
+		//
+		
+		//
+		
+		//
+		
+		//
+		
+		
+		
+		
+	}
+}
--- jtstack-nio/src/main/java/com/phei/netty/pio/TimeServerHandlerExecutePool.java
+++ jtstack-nio/src/main/java/com/phei/netty/pio/TimeServerHandlerExecutePool.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.phei.netty.pio;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -21,21 +6,18 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author Administrator
- * @date 2014年2月15日
- * @version 1.0
+ * 伪异步IO
+ * @author user
  */
 public class TimeServerHandlerExecutePool {
 
-    private ExecutorService executor;
+	private ExecutorService executor;
 
-    public TimeServerHandlerExecutePool(int maxPoolSize, int queueSize) {
-	executor = new ThreadPoolExecutor(Runtime.getRuntime()
-		.availableProcessors(), maxPoolSize, 120L, TimeUnit.SECONDS,
-		new ArrayBlockingQueue<java.lang.Runnable>(queueSize));
-    }
+	public TimeServerHandlerExecutePool(int maxPoolSize, int queueSize) {
+		executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), maxPoolSize, 120L,TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(queueSize));
+	}
 
-    public void execute(java.lang.Runnable task) {
-	executor.execute(task);
-    }
+	public void execute(Runnable task) {
+		executor.execute(task);
+	}
 }
