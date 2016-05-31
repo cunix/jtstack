f50f3d10f05b9d44c79f9876a4786f75435cc971
Wincent.Wang <cunix@sina.cn>
2016-05-31 17:27:59 +0800

    http and codec

--- jtstack-nio/src/main/java/com/phei/netty/protocol/http/xml/codec/HttpXmlRequestDecoder.java
+++ jtstack-nio/src/main/java/com/phei/netty/protocol/http/xml/codec/HttpXmlRequestDecoder.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.phei.netty.protocol.http.xml.codec;
 
 import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
@@ -29,40 +14,30 @@ import io.netty.util.CharsetUtil;
 
 import java.util.List;
 
-/**
- * @author Lilinfeng
- * @date 2014年3月1日
- * @version 1.0
- */
-public class HttpXmlRequestDecoder extends
-	AbstractHttpXmlDecoder<FullHttpRequest> {
+public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder<FullHttpRequest> {
 
-    public HttpXmlRequestDecoder(Class<?> clazz) {
-	this(clazz, false);
-    }
-
-    public HttpXmlRequestDecoder(Class<?> clazz, boolean isPrint) {
-	super(clazz, isPrint);
-    }
-
-    @Override
-    protected void decode(ChannelHandlerContext arg0, FullHttpRequest arg1,
-	    List<Object> arg2) throws Exception {
-	if (!arg1.getDecoderResult().isSuccess()) {
-	    sendError(arg0, BAD_REQUEST);
-	    return;
+	public HttpXmlRequestDecoder(Class<?> clazz) {
+		this(clazz, false);
 	}
-	HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0,
-		arg1.content()));
-	arg2.add(request);
-    }
 
-    private static void sendError(ChannelHandlerContext ctx,
-	    HttpResponseStatus status) {
-	FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
-		status, Unpooled.copiedBuffer("Failure: " + status.toString()
-			+ "\r\n", CharsetUtil.UTF_8));
-	response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
-	ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
+	public HttpXmlRequestDecoder(Class<?> clazz, boolean isPrint) {
+		super(clazz, isPrint);
+	}
+
+	@Override
+	protected void decode(ChannelHandlerContext arg0, FullHttpRequest arg1, List<Object> arg2) throws Exception {
+		if (!arg1.getDecoderResult().isSuccess()) {
+			sendError(arg0, BAD_REQUEST);
+			return;
+		}
+		HttpXmlRequest request = new HttpXmlRequest(arg1, decode0(arg0, arg1.content()));
+		arg2.add(request);
+	}
+
+	private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+		FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, status,
+				Unpooled.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8));
+		response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
+		ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+	}
 }
--- jtstack-nio/src/main/java/com/phei/netty/protocol/http/xml/server/HttpXmlServerHandler.java
+++ jtstack-nio/src/main/java/com/phei/netty/protocol/http/xml/server/HttpXmlServerHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.phei.netty.protocol.http.xml.server;
 
 import static io.netty.handler.codec.http.HttpHeaders.isKeepAlive;
@@ -40,62 +25,51 @@ import com.phei.netty.protocol.http.xml.codec.HttpXmlResponse;
 import com.phei.netty.protocol.http.xml.pojo.Address;
 import com.phei.netty.protocol.http.xml.pojo.Order;
 
-/**
- * @author lilinfeng
- * @date 2014年2月14日
- * @version 1.0
- */
-public class HttpXmlServerHandler extends
-	SimpleChannelInboundHandler<HttpXmlRequest> {
+public class HttpXmlServerHandler extends SimpleChannelInboundHandler<HttpXmlRequest> {
 
-    @Override
-    public void messageReceived(final ChannelHandlerContext ctx,
-	    HttpXmlRequest xmlRequest) throws Exception {
-	HttpRequest request = xmlRequest.getRequest();
-	Order order = (Order) xmlRequest.getBody();
-	System.out.println("Http server receive request : " + order);
-	dobusiness(order);
-	ChannelFuture future = ctx.writeAndFlush(new HttpXmlResponse(null,
-		order));
-	if (!isKeepAlive(request)) {
-	    future.addListener(new GenericFutureListener<Future<? super Void>>() {
-		public void operationComplete(Future future) throws Exception {
-		    ctx.close();
+	@Override
+	public void messageReceived(final ChannelHandlerContext ctx, HttpXmlRequest xmlRequest) throws Exception {
+		HttpRequest request = xmlRequest.getRequest();
+		Order order = (Order) xmlRequest.getBody();
+		System.out.println("Http server receive request : " + order);
+		dobusiness(order);
+		ChannelFuture future = ctx.writeAndFlush(new HttpXmlResponse(null, order));
+		if (!isKeepAlive(request)) {
+			future.addListener(new GenericFutureListener<Future<? super Void>>() {
+				public void operationComplete(Future future) throws Exception {
+					ctx.close();
+				}
+			});
 		}
-	    });
 	}
-    }
 
-    private void dobusiness(Order order) {
-	order.getCustomer().setFirstName("狄");
-	order.getCustomer().setLastName("仁杰");
-	List<String> midNames = new ArrayList<String>();
-	midNames.add("李元芳");
-	order.getCustomer().setMiddleNames(midNames);
-	Address address = order.getBillTo();
-	address.setCity("洛阳");
-	address.setCountry("大唐");
-	address.setState("河南道");
-	address.setPostCode("123456");
-	order.setBillTo(address);
-	order.setShipTo(address);
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-	    throws Exception {
-	cause.printStackTrace();
-	if (ctx.channel().isActive()) {
-	    sendError(ctx, INTERNAL_SERVER_ERROR);
+	private void dobusiness(Order order) {
+		order.getCustomer().setFirstName("狄");
+		order.getCustomer().setLastName("仁杰");
+		List<String> midNames = new ArrayList<String>();
+		midNames.add("李元芳");
+		order.getCustomer().setMiddleNames(midNames);
+		Address address = order.getBillTo();
+		address.setCity("洛阳");
+		address.setCountry("大唐");
+		address.setState("河南道");
+		address.setPostCode("123456");
+		order.setBillTo(address);
+		order.setShipTo(address);
 	}
-    }
 
-    private static void sendError(ChannelHandlerContext ctx,
-	    HttpResponseStatus status) {
-	FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
-		status, Unpooled.copiedBuffer("失败: " + status.toString()
-			+ "\r\n", CharsetUtil.UTF_8));
-	response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
-	ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		if (ctx.channel().isActive()) {
+			sendError(ctx, INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+		FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, status,
+				Unpooled.copiedBuffer("失败: " + status.toString() + "\r\n", CharsetUtil.UTF_8));
+		response.headers().set(CONTENT_TYPE, "text/plain; charset=UTF-8");
+		ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+	}
 }
